c707f99eb5f47ba1dc2014ec36111fa5094bd6c7
reidspencer <reid.spencer@yoppworks.com>
2023-12-01 13:38:04 -0500

    Attempt to fix updateFromPublicRepository & upgrade scalafmt version

--- src/main/scala/com/ossuminc/sbt/helpers/package.scala
+++ src/main/scala/com/ossuminc/sbt/helpers/package.scala
@@ -16,49 +16,50 @@ package object helpers {
     maxAge: FiniteDuration,
     log: ManagedLogger
   ): Unit = {
-    val url = URI.create(s"https://raw.githubusercontent.com/$remote").toURL
     val lastModified: Long = if (local.exists) local.lastModified() else 0L
-
-    val conn: HttpURLConnection =
-      url.openConnection().asInstanceOf[HttpURLConnection]
+    val now = System.currentTimeMillis()
+    val fiveMinutes = 5 * 60 * 1000
+    val url = URI.create(s"https://raw.githubusercontent.com/$remote").toURL
+    val conn: HttpURLConnection = url.openConnection().asInstanceOf[HttpURLConnection]
     val timeout = 30000
     conn.setConnectTimeout(timeout)
     conn.setReadTimeout(timeout)
     conn.setInstanceFollowRedirects(true)
-    conn.setIfModifiedSince(lastModified)
     conn.connect()
     val status = conn.getResponseCode
     val message = conn.getResponseMessage
-
-    status match {
-      case java.net.HttpURLConnection.HTTP_OK =>
-        Option(conn.getContent()) match {
-          case Some(is: InputStream) =>
-            val content = Source.fromInputStream(is).mkString
-            val pw = new java.io.PrintWriter(local)
-            try {
-              pw.write(content)
-            } finally {
-              pw.close()
-            }
-            log.info(
-              s"Updated .scalafmt.conf from ${url.toExternalForm}: HTTP $status $message ${Instant.ofEpochMilli(lastModified)}"
-            )
-          case Some(x: Any) =>
-            throw new IllegalStateException(
-              s"Wrong content type for ${url.toExternalForm}: ${x.getClass}."
-            )
-          case None =>
-            throw new IllegalStateException(
-              s"No content from ${url.toExternalForm}"
-            )
-        }
-      case java.net.HttpURLConnection.HTTP_NOT_MODIFIED =>
-      // hasn't been modified, so do nothing
-      case x: Int =>
-        log.warn(
-          s"Failed to get ${url.toExternalForm}: HTTP $status: $message"
-        )
+    val remoteModified: Long = conn.getLastModified
+    if (lastModified < (now - fiveMinutes) && remoteModified > lastModified) {
+      status match {
+        case java.net.HttpURLConnection.HTTP_OK =>
+          Option(conn.getContent()) match {
+            case Some(is: InputStream) =>
+              val content = Source.fromInputStream(is).mkString
+              val pw = new java.io.PrintWriter(local)
+              try {
+                pw.write(content)
+              } finally {
+                pw.close()
+              }
+              log.info(
+                s"Updated .scalafmt.conf from ${url.toExternalForm}: HTTP $status $message ${Instant.ofEpochMilli(lastModified)}"
+              )
+            case Some(x: Any) =>
+              throw new IllegalStateException(
+                s"Wrong content type for ${url.toExternalForm}: ${x.getClass}."
+              )
+            case None =>
+              throw new IllegalStateException(
+                s"No content from ${url.toExternalForm}"
+              )
+          }
+        case java.net.HttpURLConnection.HTTP_NOT_MODIFIED =>
+        // hasn't been modified, so do nothing
+        case x: Int =>
+          log.warn(
+            s"Failed to get ${url.toExternalForm}: HTTP $status: $message"
+          )
+      }
     }
   }
 
