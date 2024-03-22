49cb7e771b82deff7a9668e5452c2507965f69b3
reidspencer <reid.spencer@yoppworks.com>
2024-03-22 14:22:32 -0400

    Minor enhancements
    * Enable the plugin in all top level definitions
    * Add Program as a top level definition

--- src/main/scala/com/ossuminc/sbt/OssumIncPlugin.scala
+++ src/main/scala/com/ossuminc/sbt/OssumIncPlugin.scala
@@ -1,5 +1,8 @@
 package com.ossuminc.sbt
 
+import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
+import com.typesafe.sbt.packager.graalvmnativeimage.GraalVMNativeImagePlugin
+import com.typesafe.sbt.packager.universal.UniversalDeployPlugin
 import sbt.*
 import sbt.Keys.*
 import sbt.librarymanagement.Resolver
@@ -41,6 +44,7 @@ object OssumIncPlugin extends AutoPlugin {
       ): Project = {
         val result = Project
           .apply("root", file(System.getProperty("user.dir")))
+          .enablePlugins(OssumIncPlugin)
           .configure(
             helpers.RootProjectInfo.initialize(
               modName,
@@ -79,6 +83,7 @@ object OssumIncPlugin extends AutoPlugin {
       def apply(dirName: String, modName: String = ""): Project = {
         Project
           .apply(dirName, file(dirName))
+          .enablePlugins(OssumIncPlugin, JavaAppPackaging)
           .settings(
             name := dirName,
             moduleName := { if (modName.isEmpty) dirName else modName }
@@ -101,12 +106,33 @@ object OssumIncPlugin extends AutoPlugin {
       def apply(dirName: String, modName: String = ""): Project = {
         Project
           .apply(dirName, file(dirName))
+          .enablePlugins(OssumIncPlugin)
           .configure(helpers.Plugin.configure)
           .settings(
             name := dirName,
-            moduleName := {
-              if (modName.isEmpty) dirName else modName
-            }
+            moduleName := { if (modName.isEmpty) dirName else modName }
+          )
+      }
+    }
+
+    object Program {
+
+      /** Define a sub-project that produces an executable program. It is necessary to also have used the [[Root]]
+        * function because what that function sets up is necessary for publishing this module.
+        * @param dirName
+        *   The name of the directory in which the plugin code and tests exist
+        * @param appName
+        *   The name of the published artifact. If blank, the `dirName` will be used
+        * @return
+        *   The configured sbt project that is ready to build an sbt plugin
+        */
+      def apply(dirName: String, appName: String): Project = {
+        Project
+          .apply(dirName, file(dirName))
+          .enablePlugins(OssumIncPlugin, JavaAppPackaging, UniversalDeployPlugin, GraalVMNativeImagePlugin)
+          .settings(
+            name := dirName,
+            moduleName := { if (appName.isEmpty) dirName else appName }
           )
       }
     }
--- src/sbt-test/sbt-ossuminc/program/build.sbt
+++ src/sbt-test/sbt-ossuminc/program/build.sbt
@@ -3,8 +3,11 @@ import sbt.url
 
 enablePlugins(OssumIncPlugin)
 
-lazy val root = Root("program-test",
-  startYr = 2024,
+lazy val root = Root("everything-test",
+  org = "com.reactific",
+  orgName = "Reactific Software LLC",
+  orgPage = url("https://reactific.com/"),
+  startYr = 2014,
   devs = List(Developer(
       "reid-spencer",
       "Reid Spencer",
@@ -12,15 +15,11 @@ lazy val root = Root("program-test",
       url("https://github.com/reid-spencer")
   ))
   )
-  .configure(With.typical)
+  .configure(With.everything)
   .settings(
-    name := "program-test",
+    name := "everything-test",
     maxErrors := 50,
     TaskKey[Unit]("check") := {
       println(s"Checking from within sbt:")
     }
   )
-
-lazy val program = Program("everything", "print-hello")
-  .configure(With.typical)
-
