329247d55a474d62c8aafa44d9ec95eeef0f2277
reidspencer <reid.spencer@yoppworks.com>
2024-07-14 16:29:20 -0400

    Remove dead code

--- src/main/scala/com/ossuminc/sbt/helpers/BuildInfo.scala
+++ src/main/scala/com/ossuminc/sbt/helpers/BuildInfo.scala
@@ -63,50 +63,5 @@ object BuildInfo extends AutoPluginHelper {
           }
         )
       )
-
   }
-
-  def withBuildInfo(
-    homePage: String,
-    orgName: String,
-    packageName: String,
-    objName: String = "BuildInfo",
-    baseYear: Int = 2023
-  )(p: Project): Project = {
-    p.settings(
-      buildInfoObject := objName,
-      buildInfoPackage := packageName,
-      buildInfoOptions := Seq(ToMap, ToJson, BuildTime),
-      buildInfoUsePackageAsPath := true,
-      buildInfoKeys ++= Seq[BuildInfoKey](
-        description,
-        organization,
-        organizationName,
-        BuildInfoKey.map(organizationHomepage) { case (k, v) =>
-          k -> v.get.toString
-        },
-        BuildInfoKey.map(homepage) { case (k, v) =>
-          "projectHomepage" -> v.map(_.toString).getOrElse(homePage)
-        },
-        BuildInfoKey.map(startYear) { case (k, v) =>
-          k -> v.map(_.toString).getOrElse(baseYear.toString)
-        },
-        BuildInfoKey.map(startYear) { case (k, v) =>
-          "copyright" -> s"© ${v.map(_.toString).getOrElse(baseYear.toString)}-${Calendar
-              .getInstance()
-              .get(Calendar.YEAR)} $orgName}"
-        },
-        BuildInfoKey.map(scalaVersion) { case (k, v) =>
-          val version = if (v.head == '2') {
-            v.substring(0, v.lastIndexOf('.'))
-          } else v
-          "scalaCompatVersion" -> version
-        },
-        BuildInfoKey.map(licenses) { case (k, v) =>
-          k -> v.map(_._1).mkString(", ")
-        }
-      )
-    )
-  }
-
 }
