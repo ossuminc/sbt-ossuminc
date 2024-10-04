701263a63bbd7a62b9d4c7ee33e8694cfd0a426e
reidspencer <reid.spencer@yoppworks.com>
2024-10-04 10:46:34 -0400

    Fix importation of non-JVM dependency

--- src/main/scala/com/ossuminc/sbt/helpers/Riddl.scala
+++ src/main/scala/com/ossuminc/sbt/helpers/Riddl.scala
@@ -9,15 +9,13 @@ object Riddl extends AutoPluginHelper {
   override def configure(project: Project): Project =
     configure()(project)
 
-  def configure(version: String = "0.52.1")(project: Project): Project =
+  def configure(version: String = "0.52.1", nonJVMDependency: Boolean = true)(project: Project): Project =
     project.settings(
       libraryDependencies += {
-        if (project.id.endsWith("JS")) {
+        if (nonJVMDependency) {
           "com.ossuminc" %%% "riddl-diagrams" % version
-        } else if (project.id.endsWith("Native")) {
-          "com.ossuminc" %%% "riddl-commands" % version
         } else {
-          "com.ossuminc" %% "riddl" % version
+          "com.ossuminc" %% "riddl-commands" % version
         }
       }
     )
